b46bc2dde88f7527ee70d6409f2e5c6ccf7ceaef
Nicolas Peters <peters.nico@gmail.com>
2016-03-31 23:33:54 +0200

    agent map + filter java process

--- offcpu.sh
+++ offcpu.sh
@@ -1,8 +1,12 @@
 #!/bin/env bash
-sudo perf record -e sched:sched_stat_sleep -e sched:sched_switch  -e sched:sched_process_exit -a -g -o /tmp/perf-offcpu.data.raw sleep 5
-sudo perf inject -v -s -f -i perf-offcpu.data.raw -o /tmp/perf-offcpu.data
-perf script -i /tmp/perf-offcpu.data   -F comm,pid,tid,cpu,time,period,event,ip,sym,dso,trace | awk '
+PID=$1
+sudo perf sched  record -a -g -o /tmp/perf-offcpu.data.raw sleep 15 
+
+/opt/perf-map-agent/bin/create-java-perf-map.sh $PID 
+
+sudo perf inject -v -s -f -i /tmp/perf-offcpu.data.raw -o /tmp/perf-offcpu.data
+sudo perf script -i /tmp/perf-offcpu.data   -F comm,pid,tid,cpu,time,period,event,ip,sym,dso,trace | awk '
     NF > 4 { exec = $1; period_ms = int($5 / 1000000) }
-    NF > 1 && NF <= 4 && period_ms > 0 { print $2 }
-    NF < 2 && period_ms > 0 { printf "%s\n%d\n\n", exec, period_ms }' | $FLAMEGRAPH_DIR/FlameGraph/stackcollapse.pl |\
-     $FLAMEGRAPH_DIR/FlameGraph/flamegraph.pl --countname=ms --title="Off-CPU Time Flame Graph" --colors=io > flamegraph-output/cpuoff.svg
+    NF > 1 && NF <= 4 && period_ms > 0 {  str=$2;  sub(/;/, "", str);  print str }
+    NF < 2 && period_ms > 0 { printf "%s\n%d\n\n", exec, period_ms }' | $FLAMEGRAPH_DIR/stackcollapse.pl | grep java  | grep -vi safepoint | \
+     $FLAMEGRAPH_DIR/flamegraph.pl --countname=ms --title="Off-CPU Time Flame Graph" --colors=io > cpuoff.svg
--- sample-flamegraph-app/src/main/java/sample/flamegraph/web/ProxyController.java
+++ sample-flamegraph-app/src/main/java/sample/flamegraph/web/ProxyController.java
@@ -12,16 +12,16 @@ import java.net.URL;
 @RequestMapping("/proxy")
 public class ProxyController {
 
+    private static String REMOTE_SERVER = "192.168.33.1";
+
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public int proxy()  throws Exception{
 
-        String url = "http://localhost:9000/remote";
+        String url = String.format("http://%s:9000/remote", REMOTE_SERVER);
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        // optional default is GET
         con.setRequestMethod("GET");
-
         int responseCode = con.getResponseCode();
         return con.getResponseCode();
     }
